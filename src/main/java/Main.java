67fea32b731b699c082ad46c2d40c2fe8d45ecf2
PatrickA91 <90562401+PatrickA91@users.noreply.github.com>
2022-10-14 16:38:34 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,20 @@
 public class Main {
-    public static boolean isLeapYear(int year) {
+    int i;
+    public Main() {
+        this.i = 1;
+    }
+
+    public boolean isLeapYear(int year) {
         if (year % 400 == 0){
             return true;
         }
-        if (year % 4 == 0){
-            if (year % 300 != 0){
-                return true;
+        else if (year % 100 == 0) {
+                return false;
             }
+        else if (year % 4 == 0) {
+            return true;
         }
-        return false;
+    return false;
     }
+
 }
